24bdd3fff807a2ecb86f945f737a6c49b32b2560
krishan <astrohari09@outlook.com>
2026-03-23 17:16:13 +0530

    Update start.py

--- SACHINxSPAM/modules/start.py
+++ SACHINxSPAM/modules/start.py
@@ -4,18 +4,18 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_OP = [
     [
-        Button.url("🍁 sᴀᴄʜɪɴ", "https://t.me/V_VIP_OWNER"),
-        Button.url("ᴜsᴇʀʙᴏᴛ 🕸️", "https://t.me/SANATANI_X_ROBOT")
+        Button.url("🍁 sᴀᴄʜɪɴ", "➻❥⏤͟͞ᴋʀɪsʜᴀɴ💸⎯ꨄ"),
+        Button.url("ᴜsᴇʀʙᴏᴛ 🕸️", "https://t.me/cutieee_musicbot")
     ],
     [
         Button.inline("🥀 ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅs 🥀", data="help_back")
     ],
     [
-        Button.url("✨ ᴜᴘᴅᴀᴛᴇ", "https://t.me/All_SANATANI_BOT"),
-        Button.url("sᴜᴘᴘᴏʀᴛ ❄️", "https://t.me/+Ckzm2ypQyIIzZTll")
+        Button.url("✨ ᴜᴘᴅᴀᴛᴇ", "https://t.me/+PC-rxWT0YD1jM2Q9"),
+        Button.url("sᴜᴘᴘᴏʀᴛ ❄️", "https://t.me/+PC-rxWT0YD1jM2Q9")
     ],
     [
-        Button.url("🌸 ᴊᴏɪɴ ғᴏʀ sᴜᴅᴏ 🌸", "https://t.me/+Ckzm2ypQyIIzZTll")
+        Button.url("🌸 ᴊᴏɪɴ ғᴏʀ sᴜᴅᴏ 🌸", "https://t.me/+PC-rxWT0YD1jM2Q9")
     ],
 ]
 
@@ -34,10 +34,10 @@ async def start(event):
         KEX = await event.client.get_me()
         bot_name = KEX.first_name
         bot_id = KEX.id
-        TEXT = f"**╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ⏤͟͟͞͞‌‌‌‌★**\n**┆**\n**┊◍ ʜᴇʏ : [{event.sender.first_name}] **\n**┆◍ ɪ ᴀᴍ : [{bot_name}](tg://user?id={bot_id}) **\n**┊**\n**┆● sᴀɴᴀᴛᴀɴɪ ʙᴏᴛ ᴠᴇʀsɪᴏɴ :** `0.2`\n**┊● ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `8.2.5.1.01`\n**╰─────────────────────────**\n**──────────────────────────**\n**⦿ Oᴡɴᴇʀ - [🦋⃟‌🇸ʌᷟᴄᷣʜɪ֟፝η 🌸](https://t.me/v_vip_owner) | [🦋⃟‌🇸ʌᷟᴄᷣʜɪ֟፝η 🌸](https://t.me/sachin_owner) **\n**──────────────────────────**\n**    ❖ Uᴘᴅᴀᴛᴇ's ⏤͟͟͞͞‌‌‌‌ [❖ ∣ Sᴀɴᴀᴛᴀɴɪ Tᴇᴄʜ ∣ ❖](https://t.me/all_sanatani_bot) **\n**──────────────────────────**"
+        TEXT = f"**╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ⏤͟͟͞͞‌‌‌‌★**\n**┆**\n**┊◍ ʜᴇʏ : [{event.sender.first_name}] **\n**┆◍ ɪ ᴀᴍ : [{bot_name}](tg://user?id={bot_id}) **\n**┊**\n**┆● ʙᴏᴛ ᴠᴇʀsɪᴏɴ :** `0.2`\n**┊● ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `8.2.5.1.01`\n**╰─────────────────────────**\n**──────────────────────────**\n**⦿ Oᴡɴᴇʀ - [➻❥⏤͟͞ᴋʀɪsʜᴀɴ💸⎯ꨄ](https://t.me/censored_politicss) | [➻❥⏤͟͞ᴋʀɪsʜᴀɴ💸⎯ꨄ](https://t.me/censored_krishan) **\n**──────────────────────────**\n**    ❖ Uᴘᴅᴀᴛᴇ's ⏤͟͟͞͞‌‌‌‌ [𝚅𝙸𝙱𝙴𝚂 𝙷𝚄𝙱](https://t.me/+PC-rxWT0YD1jM2Q9) **\n**──────────────────────────**"
         await event.client.send_file(
                     event.chat_id,  
-                    "https://telegra.ph//file/7cfeff721589b61a2f634.jpg",
+                    "https://pub-404d3d1646bf4caab306e0c1451fe7f9.r2.dev/photo_2024-12-26_19-48-24.jpg",
                     caption=TEXT, 
                     buttons=START_OP
                 )
